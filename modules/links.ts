8941b07f7f4b45e7ec5a5375534cd722cf210bd0
YandeMC <YandeMC@users.noreply.github.com>
2024-02-10 18:54:11 +0000

    Format

--- modules/links.ts
+++ modules/links.ts
@@ -1,10 +1,15 @@
 import { defineEvent } from "strife.js";
 
 defineEvent("messageCreate", async (m) => {
-   if (m.content.includes("https://discord.gg/") || m.content.includes("https://discord.com/invite/")) {
-     if (!(m.member?.roles.cache.some(role => role.id === '1193758457493471253'))) {
-       await m.delete()
-       await m.channel.send({content: `<@${m.author.id}>, Only verified users can send discord invite links!`})
-    }
-}
-})
+	if (
+		m.content.includes("https://discord.gg/") ||
+		m.content.includes("https://discord.com/invite/")
+	) {
+		if (!m.member?.roles.cache.some((role) => role.id === "1193758457493471253")) {
+			await m.delete();
+			await m.channel.send({
+				content: `<@${m.author.id}>, Only verified users can send discord invite links!`,
+			});
+		}
+	}
+});
