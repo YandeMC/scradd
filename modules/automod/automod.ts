5c538ef9511a6df1d17fe18508f499348626f1b0
Yande <113806118+YandeMC@users.noreply.github.com>
2024-02-27 17:33:57 -0500

    Add image censoring

--- modules/automod/automod.ts
+++ modules/automod/automod.ts
@@ -1,73 +1,90 @@
-import { Constants, type Message } from "discord.js";
+import { Constants, type MessageType, type Message } from "discord.js";
 import { client } from "strife.js";
 import config from "../../common/config.js";
 import constants from "../../common/constants.js";
 import {
+	getBaseChannel,
 	GlobalAnimatedEmoji,
 	GlobalBotInvitesPattern,
 	InvitesPattern,
-	getBaseChannel,
 } from "../../util/discord.js";
-import { stripMarkdown } from "../../util/markdown.js";
-import { joinWithAnd } from "../../util/text.js";
 import log, { LogSeverity, LoggingErrorEmoji } from "../logging/misc.js";
 import { PARTIAL_STRIKE_COUNT } from "../punishments/misc.js";
 import warn from "../punishments/warn.js";
-import { getLevelForXp } from "../xp/misc.js";
-import { xpDatabase } from "../xp/util.js";
 import tryCensor, { badWordRegexps, badWordsAllowed } from "./misc.js";
+import { stripMarkdown } from "../../util/markdown.js";
+import { joinWithAnd } from "../../util/text.js";
+import { createWorker } from "tesseract.js";
+const worker = await createWorker("eng");
+async function getMessageImageText(message: Message): Promise<string> {
+	if (message.attachments.size === 0) {
+	  return ""; // No images
+	}
+  
+	const imageUrls: string[] = Array.from(message.attachments.values())
+	  .filter((attachment) => attachment.width && attachment.height)
+	  .map((attachment) => attachment.url);
+  
+	if (imageUrls.length === 0) {
+	  return "";
+	}
+  
+	const imageTextPromises: Promise<string>[] = imageUrls.map(async (url) => {
+	  const ret = await worker.recognize(url || "");
+	  return ret.data.text;
+	});
+  
+	const imageTextResults = await Promise.all(imageTextPromises);
+	return imageTextResults.join(" ");
+  }
+  
 
-export default async function automodMessage(message: Message): Promise<boolean> {
+const WHITELISTED_INVITE_GUILDS = new Set([
+	config.guild.id,
+	...config.otherGuildIds,
+	"898383289059016704", // Scratch Addons SMP Archive
+	"837024174865776680", // TurboWarp
+	"945340853189247016", // ScratchTools
+	"461575285364752384", // 9th Tail Bot Hub
+	"333355888058302465", // DISBOARD
+	undefined, // Invalid links
+]);
+
+export default async function automodMessage(message: Message) {
 	const allowBadWords = badWordsAllowed(message.channel);
 	const baseChannel = getBaseChannel(message.channel);
 	const pings = message.mentions.users.size
-		? ` (ghost pinged ${joinWithAnd(message.mentions.users.map((user: { toString: () => any; }) => user.toString()))})`
+		? ` (ghost pinged ${joinWithAnd(message.mentions.users.map((user) => user.toString()))})`
 		: "";
 
-	let needsDelete = false;
-	let deletionMessage = "";
-
-	const animatedEmojis =
-		baseChannel?.id !== config.channels.bots?.id && message.content.match(GlobalAnimatedEmoji);
+	const animatedEmojis = message.content.match(GlobalAnimatedEmoji);
 	const badAnimatedEmojis =
 		animatedEmojis &&
 		animatedEmojis.length > 15 &&
 		Math.floor((animatedEmojis.length - 16) / 10) * PARTIAL_STRIKE_COUNT;
 
-	if (animatedEmojis && typeof badAnimatedEmojis === "number") {
+	let needsDelete = false;
+
+	if (baseChannel?.id !== config.channels.bots?.id && typeof badAnimatedEmojis === "number") {
 		needsDelete = true;
 		await warn(
 			message.author,
-			`${animatedEmojis.length} animated emojis`,
+			"Please don’t post that many animated emojis!",
 			badAnimatedEmojis,
-			animatedEmojis.join(""),
+			animatedEmojis?.join(""),
 		);
-		deletionMessage += ` Please don’t post that many animated emojis!`;
-	}
-
-	if (allowBadWords) {
-		if (!needsDelete) return true;
-		if (!message.deletable) {
-			await log(
-				`${LoggingErrorEmoji} Unable to delete ${message.url} (${deletionMessage.trim()})`,
-				LogSeverity.Alert,
-			);
-			return true;
-		}
-
-		const publicWarn = await message.reply({
-			content: `${constants.emojis.statuses.no}${deletionMessage}${pings}`,
-			allowedMentions: { users: [], repliedUser: true },
+		await message.channel.send({
+			content: `${
+				constants.emojis.statuses.no
+			} ${message.author.toString()}, less animated emojis please!${pings}`,
+			allowedMentions: { users: [message.author.id] },
 		});
-		await message.delete();
-		setTimeout(() => publicWarn.delete(), 300_000);
-		return false;
 	}
 
-	const inviteLinks = message.content.match(InvitesPattern) ?? [];
+	const links = message.content.match(InvitesPattern) ?? [];
 	const invites = await Promise.all(
-		inviteLinks.map(
-			async (link: string) =>
+		links.map(
+			async (link) =>
 				[
 					link,
 					await client.fetchInvite(link.split("/").at(-1) ?? link).catch(() => void 0),
@@ -75,144 +92,144 @@ export default async function automodMessage(message: Message): Promise<boolean>
 		),
 	);
 
-	if (
-		config.channels.advertise &&
-		baseChannel &&
-		config.channels.advertise.id !== baseChannel.id &&
-		!baseChannel.isDMBased() &&
-		baseChannel.permissionsFor(baseChannel.guild.id)?.has("SendMessages")
-	) {
-		const badInvites = [
-			...new Set(
-				invites
-					
-					.map(([link]) => link),
-			),
-		];
+	if (!allowBadWords) {
+		if (
+			!message.author.bot &&
+			config.channels.advertise &&
+			(config.channels.advertise.id !== baseChannel?.id ||
+				(!baseChannel.isDMBased() &&
+					baseChannel.permissionsFor(baseChannel.guild.id)?.has("SendMessages")))
+		) {
+			const badInvites = [
+				...new Set(
+					invites
+						.filter(([, invite]) => !WHITELISTED_INVITE_GUILDS.has(invite?.guild?.id))
+						.map(([link]) => link),
+				),
+			];
 
-		if (badInvites.length) {
-			needsDelete = true;
-			await warn(
-				message.author,
-				`Server invite in ${message.channel.toString()}`,
-				badInvites.length,
-				badInvites.join("\n"),
-			);
-			deletionMessage += ` Please keep server invites in ${config.channels.advertise.toString()}!`;
-		}
-
-		const bots = [...new Set(message.content.match(GlobalBotInvitesPattern))];
-		if (!message.author.bot && bots.length) {
-			needsDelete = true;
-			await warn(
-				message.author,
-				`Bot invite in ${message.channel.toString()}`,
-				bots.length,
-				bots.join("\n"),
-			);
-			deletionMessage += ` Please don’t post bot invites outside of ${config.channels.advertise.toString()}!`;
-		}
-
-		if (baseChannel.name.includes("general") || baseChannel.name.includes("showcase")) {
-			const links = Array.from(
-				new Set(message.content.match(/(https?:\/\/[^\s"')*,.:;<>\]]+)/gis) ?? []),
-				(link) => new URL(link),
-			).filter((link) =>
-				[
-					"scratch.camp",
-					"scratch.love",
-					"scratch.mit.edu",
-					"scratch.org",
-					"scratch.pizza",
-					"scratch.team",
-
-					"youtu.be",
-					"youtube.com",
-				].includes(link.hostname),
-			);
-
-			const level = getLevelForXp(
-				xpDatabase.data.find(({ user }) => user === message.author.id)?.xp ?? 0,
-			);
-			const canPostLinks =
-				!links.length ||
-				level > 4 ||
-				[(config.roles.dev?.id, config.roles.epic?.id, config.roles.booster?.id)].some(
-					(role) => !message.member || (role && message.member.roles.resolve(role)),
-				);
-
-			if (!canPostLinks) {
+			if (badInvites.length) {
 				needsDelete = true;
 				await warn(
 					message.author,
-					`Posted blacklisted link${
-						links.length === 1 ? "" : "s"
-					} in ${message.channel.toString()} while at level ${level}`,
-					links.length * 0.25,
-					links.join(" "),
+					"Please don’t send server invites in that channel!",
+					badInvites.length,
+					badInvites.join("\n"),
 				);
-				deletionMessage += ` Sorry, but you need level 5 to post ${
-					links.length === 1 ? "that link" : "those links"
-				} outside a channel like ${config.channels.advertise.toString()}!`;
+				await message.channel.send({
+					content: `${
+						constants.emojis.statuses.no
+					} ${message.author.toString()}, only post invite links in ${config.channels.advertise.toString()}!${pings}`,
+					allowedMentions: { users: [message.author.id] },
+				});
+			}
+
+			const bots = [...new Set(message.content.match(GlobalBotInvitesPattern))];
+			if (bots.length) {
+				needsDelete = true;
+				await warn(
+					message.author,
+					"Please don’t post bot invite links!",
+					bots.length,
+					bots.join("\n"),
+				);
+				await message.channel.send({
+					content: `${
+						constants.emojis.statuses.no
+					} ${message.author.toString()}, bot invites go to ${config.channels.advertise.toString()}!${pings}`,
+					allowedMentions: { users: [message.author.id] },
+				});
 			}
 		}
-	}
 
-	const badWords = [
-		tryCensor(stripMarkdown(message.content)),
-		...message.stickers.map(({ name }) => tryCensor(name)),
-		...invites.map(([, invite]) => !!invite?.guild && tryCensor(invite.guild.name)),
-	].reduce(
-		(bad, censored) =>
-			typeof censored === "boolean"
-				? bad
-				: {
-						strikes: bad.strikes + censored.strikes,
-						words: bad.words.map((words: any, index: number) => [
-							...words,
-							...(censored.words[index] ?? []),
-						]),
-				  },
-		{ strikes: 0, words: Array.from<string[]>({ length: badWordRegexps.length }).fill([]) },
-	);
-	if (badWords.strikes) needsDelete = true;
-
-	
-
-	if (
-		!([...Constants.NonSystemMessageTypes] as const).includes(message.type)
-	)
-		needsDelete = true;
-
-	const languageStrikes = badWords.strikes;
-	if (languageStrikes) {
-		const words = [...badWords.words.flat()];
-		await warn(
-			message.interaction?.user ?? message.author,
-			words.length === 1 ? "Used a banned word" : "Used banned words",
-			languageStrikes,
-			words.join(", "),
+		const badWords = [
+			tryCensor(stripMarkdown(message.content)),
+			tryCensor(await getMessageImageText(message)),
+			...message.stickers.map(({ name }) => tryCensor(name)),
+			...invites.map(([, invite]) => !!invite?.guild && tryCensor(invite.guild.name)),
+		].reduce(
+			(bad, censored) =>
+				typeof censored === "boolean"
+					? bad
+					: {
+							strikes: bad.strikes + censored.strikes,
+							words: bad.words.map((words, index) => [
+								...words,
+								...(censored.words[index] ?? []),
+							]),
+					  },
+			{ strikes: 0, words: Array.from<string[]>({ length: badWordRegexps.length }).fill([]) },
 		);
-		deletionMessage +=
-			languageStrikes < 1 ? " Please don’t say that here!" : " Please watch your language!";
-	}
+		if (badWords.strikes) needsDelete = true;
 
-	if (needsDelete) {
-		if (message.deletable) {
-			const publicWarn = await message.reply({
-				content: `${constants.emojis.statuses.no}${deletionMessage}${pings}`,
-				allowedMentions: { users: [], repliedUser: true },
-			});
-			await message.delete();
-			setTimeout(() => publicWarn.delete(), 300_000);
-			return false;
+		const badEmbedWords = message.embeds
+			.flatMap((embed) => [
+				embed.description,
+				embed.title,
+				embed.footer?.text,
+				embed.author?.name,
+				...embed.fields.flatMap((field) => [field.name, field.value]),
+			])
+			.reduce(
+				(bad, current) => {
+					const censored = tryCensor(current || "", 1);
+					return censored
+						? {
+								strikes: bad.strikes + censored.strikes,
+								words: bad.words.map((words, index) => [
+									...words,
+									...(censored.words[index] ?? []),
+								]),
+						  }
+						: bad;
+				},
+				{
+					strikes: 0,
+					words: Array.from<string[]>({ length: badWordRegexps.length }).fill([]),
+				},
+			);
+
+		if (
+			badEmbedWords.strikes &&
+			!(Constants.NonSystemMessageTypes as MessageType[]).includes(message.type)
+		)
+			needsDelete = true;
+
+		const languageStrikes = badWords.strikes + badEmbedWords.strikes;
+		if (needsDelete) {
+			if (message.deletable) {
+				await message.delete();
+				if (badWords.strikes)
+					await message.channel.send({
+						content: `${constants.emojis.statuses.no} ${message.author.toString()}, ${
+							languageStrikes < 1 ? "that’s not appropriate" : "language"
+						}!${pings}`,
+						allowedMentions: { users: [message.author.id] },
+					});
+			} else {
+				await log(
+					`${LoggingErrorEmoji} Missing permissions to delete ${message.url}`,
+					LogSeverity.Alert,
+				);
+			}
+		} else if (badEmbedWords.strikes) {
+			await message.suppressEmbeds();
+			await message.reply(
+				`${constants.emojis.statuses.no} ${message.author.toString()}, ${
+					languageStrikes < 1 ? "that’s not appropriate" : "language"
+				}!`,
+			);
 		}
 
-		await log(
-			`${LoggingErrorEmoji} Unable to delete ${message.url} (${deletionMessage.trim()})`,
-			LogSeverity.Alert,
-		);
+		if (badWords.strikes || badEmbedWords.strikes) {
+			await warn(
+				message.interaction?.user ?? message.author,
+				"Please watch your language!",
+				languageStrikes,
+				[...badEmbedWords.words.flat(), ...badWords.words.flat()].join(", "),
+			);
+		}
 	}
 
-	return true;
+	return !needsDelete;
 }
