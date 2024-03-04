4f0ce644780ee913832086a042db077aa9e2c80c
Yande <113806118+YandeMC@users.noreply.github.com>
2024-03-03 20:56:51 -0500

    asgsdthsdgsgrsegfserraew

--- common/constants.ts
+++ common/constants.ts
@@ -17,7 +17,7 @@ export default {
 	collectorTime: 45_000,
 	defaultPunishment: "No reason given.",
 
-	emojis: {
+	/*emojis: {
 		discord: {
 			add: "<:add:1195859865579561052>",
 			boost: "<:boost:1195860016427696138>",
@@ -65,8 +65,71 @@ export default {
 			join: "<:join:1193656153666748548>",
 			leave: "<:leave:1193656172297855026>",
 		},
-	},
+	},*/
 
+	emojis: {
+		message: {
+			add: "<:add:1195859865579561052>",
+			boost: "<:boost:1195860016427696138>",
+			call: "<:call:1195858170309001307>",
+			deafened: "<:deafened:1195857887155728424>",
+			edit: "<:edit:1195857904100708392> ",
+			error: "<:error:1193656283396579418>",
+			muted: "<:muted:1195857919057612822>",
+			no: "<:no:1193656110696112259>",
+			pin: "<:pin:1195858189871226922>",
+			raisedHand: "<:raised_hand:1195858087853170790>",
+			remove: "<:remove:1195859908978016316>",
+			reply: "<:reply:1195858012171161631>",
+			speaker: "<:speaker:1195858063211643070>",
+			stage: "<:stage:1195860523967840347>",
+			stageLive: "<:stage_live:1195860538182336793>",
+			streaming: "<:streaming:1195860552908558498>",
+			thread: "<:thread:1195858146724433990>",
+			typing: "<a:typing:1195857946156994711>",
+			warning: "<:warning:1193656265520459806>",
+			yes: "<:yes:1193656129750847488>",
+			subscription: "",
+			live: "",
+			fail: "",
+			success: "",
+			
+		},
+
+		misc: {
+			addon: "<:_:817273401869205524>",
+			blue: "<:primary:1195858102952665268>",
+			green: "<:success:1195857992814448681>",
+			loading: "<a:_:949436374174560276>",
+		},
+
+		scratch: {
+			comments: "<:Comments:1101507674442584145>",
+			favorite: "<:Star:1101507667857526784>",
+			followers: "<:People:1101507672852934837>",
+			following: "<:Person:1101507670860632235>",
+			love: "<:Heart:1101507665802301560>",
+			projects: "<:projects:1101507677894488174>",
+			remix: "<:Remix:1101507669489107024>",
+			view: "<:View:1101509288221999306>",
+		},
+
+		statuses: { no: "<:no:1193656110696112259>", yes: "<:yes:1193656129750847488>" },
+
+		vc: {
+			camera: "<:_:1202777712997961768>",
+			deafened: "<:deafened:1195857887155728424>",
+			muted: "<:muted:1195857919057612822>",
+			streaming: "<:streaming:1195860552908558498>",
+		},
+
+		welcome: {
+			ban: "<:ban:1193655180630184047>",
+			kick: "<:kick:1204568132107247646>",
+			join: "<:join:1193656153666748548>",
+			leave: "<:leave:1193656172297855026>",
+		},
+	},
 	fonts: "Sora, SoraExt, sans-serif",
 	footerSeperator: " • ",
 	scratchColor: 0x88_5c_d4,
