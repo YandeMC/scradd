1965eb0faed2aab5cb2c365c7521c9e38b6dbb3c
TheYande <TheYande@users.noreply.github.com>
2024-04-20 19:34:17 +0000

    Format

--- modules/translate.ts
+++ modules/translate.ts
@@ -2,20 +2,26 @@ import { translate } from "@vitalets/google-translate-api";
 import { defineEvent } from "strife.js";
 
 defineEvent("messageReactionAdd", async (reaction) => {
-    if (reaction.emoji.name) {
-        const message = reaction.message;
-        const flagCode = reaction.emoji.name
-        const language = getLanguageForCountryCode(flagCode); 
-        if (language && message.content) {
-            try {
-                const translatedText = (await translate(message.content, { to: language }));
-                message.reply({content: `${flagCode.toUpperCase()}: ${translatedText.text}`, allowedMentions: {users:[]}});
-            } catch (error) {
-                console.error('Error translating message:', error);
-                message.reply({content: 'There was an error trying to translate the message.', allowedMentions: {users:[]}});
-            }
-        }
-    }
+	if (reaction.emoji.name) {
+		const message = reaction.message;
+		const flagCode = reaction.emoji.name;
+		const language = getLanguageForCountryCode(flagCode);
+		if (language && message.content) {
+			try {
+				const translatedText = await translate(message.content, { to: language });
+				message.reply({
+					content: `${flagCode.toUpperCase()}: ${translatedText.text}`,
+					allowedMentions: { users: [] },
+				});
+			} catch (error) {
+				console.error("Error translating message:", error);
+				message.reply({
+					content: "There was an error trying to translate the message.",
+					allowedMentions: { users: [] },
+				});
+			}
+		}
+	}
 });
 
 function getLanguageForCountryCode(code: string) {
