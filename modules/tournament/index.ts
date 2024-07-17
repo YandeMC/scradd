c1cf8d3a1ff0db9f4cbf2f601583e9f78ee343f7
Yande <yande@yande.dev>
2024-07-17 14:26:15 -0400

    a

--- modules/tournament/index.ts
+++ modules/tournament/index.ts
@@ -6,21 +6,22 @@ findMatch;
 
 
 defineChatCommand({
-    name:"tournament-join",
-    description:"Join the RPS Tournament"
+    name: "tournament-join",
+    description: "Join the RPS Tournament"
 }, async (i) => {
+    await i.deferReply()
     const result = await addParticipant(formatUser(i.user))
-    if (result) await       (await i.guild?.members.fetch(i.user.id))?.roles.add("1261372320602259458")
-    i.reply(result ? "## Success\nYou have been sucessfully added to the tournament" : "## Error\nSomething went wrong when adding you to the tournament")
+    if (result) await (await i.guild?.members.fetch(i.user.id))?.roles.add("1261372320602259458")
+    await i.reply(result ? "## Success\nYou have been sucessfully added to the tournament" : "## Error\nSomething went wrong when adding you to the tournament")
 })
 
 
 defineChatCommand({
-    name:"tournament-leave",
-    description:"Leave the RPS  "
+    name: "tournament-leave",
+    description: "Leave the RPS Tournament"
 }, async (i) => {
-    i.deferReply()
+    await i.deferReply()
     const result = await removeParticipant(i.user.id)
     if (result) await (await i.guild?.members.fetch(i.user.id))?.roles.remove("1261372320602259458")
-    i.editReply(result ? "## Success\nYou have been sucessfully removed from the tournament" : "## Error\nSomething went wrong when removing you from the tournament")
+    await i.editReply(result ? "## Success\nYou have been sucessfully removed from the tournament" : "## Error\nSomething went wrong when removing you from the tournament")
 })
