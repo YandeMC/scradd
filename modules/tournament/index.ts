bb4491a8dd3c3476c97458f142a8f65bbcfcffab
TheYande <TheYande@users.noreply.github.com>
2024-07-17 23:55:56 +0000

    Format

--- modules/tournament/index.ts
+++ modules/tournament/index.ts
@@ -5,27 +5,41 @@ addParticipant;
 removeParticipant;
 findMatch;
 
+defineChatCommand(
+	{
+		name: "tournament-join",
+		description: "Join the RPS Tournament",
+	},
+	async (i) => {
+		await i.deferReply();
+		const result = await addParticipant(formatUser(i.user));
+		if (result)
+			await (await i.guild?.members.fetch(i.user.id))?.roles.add("1261372320602259458");
+		await i.editReply(
+			result ?
+				"## Success\nYou have been sucessfully added to the tournament"
+			:	"## Error\nSomething went wrong when adding you to the tournament",
+		);
+	},
+);
 
-defineChatCommand({
-    name: "tournament-join",
-    description: "Join the RPS Tournament"
-}, async (i) => {
-    await i.deferReply()
-    const result = await addParticipant(formatUser(i.user))
-    if (result) await (await i.guild?.members.fetch(i.user.id))?.roles.add("1261372320602259458")
-    await i.editReply(result ? "## Success\nYou have been sucessfully added to the tournament" : "## Error\nSomething went wrong when adding you to the tournament")
-})
-
-
-defineChatCommand({
-    name: "tournament-leave",
-    description: "Leave the RPS Tournament"
-}, async (i) => {
-    await i.deferReply()
-    const result = await removeParticipant(i.user.id)
-    if (result) await (await i.guild?.members.fetch(i.user.id))?.roles.remove("1261372320602259458")
-    await i.editReply(result ? "## Success\nYou have been sucessfully removed from the tournament" : "## Error\nSomething went wrong when removing you from the tournament")
-})
+defineChatCommand(
+	{
+		name: "tournament-leave",
+		description: "Leave the RPS Tournament",
+	},
+	async (i) => {
+		await i.deferReply();
+		const result = await removeParticipant(i.user.id);
+		if (result)
+			await (await i.guild?.members.fetch(i.user.id))?.roles.remove("1261372320602259458");
+		await i.editReply(
+			result ?
+				"## Success\nYou have been sucessfully removed from the tournament"
+			:	"## Error\nSomething went wrong when removing you from the tournament",
+		);
+	},
+);
 
 // defineChatCommand({
 //     name: "tournament-opponent",
@@ -44,4 +58,4 @@ defineChatCommand({
 //             }
 //         ]
 //     })
-// })                          
+// })
