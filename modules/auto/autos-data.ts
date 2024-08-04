61c379b087cd34da08bedd2e696a76cad3d1751b
TheYande <TheYande@users.noreply.github.com>
2024-08-04 06:16:09 +0000

    Format

--- modules/auto/autos-data.ts
+++ modules/auto/autos-data.ts
@@ -19,7 +19,7 @@ export const customResponses: Record<string, string> = {
 	Gay: "yeah like yan- why do i hear boss music",
 	Zuzu: "https://cdn.discordapp.com/attachments/1141222490597757025/1269535264037011478/zuzu.png",
 	Yande: "(    b    )",
-	Trans: "<:grableft:1241127094356803714><:Estrogen:1269538313233240137><:grabright:1241127108852449312>"
+	Trans: "<:grableft:1241127094356803714><:Estrogen:1269538313233240137><:grabright:1241127108852449312>",
 };
 export const customNames: Record<string, string> = {
 	Abaka: "YandeAI Banner",
