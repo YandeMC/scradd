28b031da7b8e2f567528c9a6081387edf4409f23
TheYande <TheYande@users.noreply.github.com>
2024-08-07 04:54:26 +0000

    Format

--- modules/giveaway.ts
+++ modules/giveaway.ts
@@ -52,9 +52,18 @@ defineChatCommand(
 	},
 
 	async (int, options) => {
-		if (options.prizes.includes("_")) return await int.reply({ ephemeral: true, content: "Due to the way giveaways are stored, underscores are not allowed in the prize input." })
-		const prizes = options.prizes.split(",").map((p) => p.trim()).filter(p => !!p);
-		if (prizes.length > 8) return int.reply({ephemeral:true, content:"The maximum number of prizes is 8."})
+		if (options.prizes.includes("_"))
+			return await int.reply({
+				ephemeral: true,
+				content:
+					"Due to the way giveaways are stored, underscores are not allowed in the prize input.",
+			});
+		const prizes = options.prizes
+			.split(",")
+			.map((p) => p.trim())
+			.filter((p) => !!p);
+		if (prizes.length > 8)
+			return int.reply({ ephemeral: true, content: "The maximum number of prizes is 8." });
 		const date = parseTime(options.time);
 		if (+date < Date.now() + 60_000 || +date > Date.now() + 31_536_000_000) {
 			return await int.reply({
@@ -95,7 +104,7 @@ defineChatCommand(
 								url: options.image.url,
 							},
 						}
-						: {}),
+					:	{}),
 					fields:
 						options["role-required"] ?
 							[
@@ -104,7 +113,7 @@ defineChatCommand(
 									value: options["role-required"].toString(),
 								},
 							]
-							: [],
+						:	[],
 				},
 			],
 		});
