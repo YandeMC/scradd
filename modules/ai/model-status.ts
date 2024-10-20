fa1b9f31d96134d22f9b3af950f294a9313e42b0
cicerorph <cicerorph@users.noreply.github.com>
2024-10-20 00:55:11 +0000

    Format

--- modules/ai/model-status.ts
+++ modules/ai/model-status.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 import { EmbedBuilder } from "discord.js";
 import { client } from "strife.js";
 let messageId: string | null = null;
-const models = [
-	{ supportsComplex: true, name: "@cf/meta/llama-3.2-11b-vision-instruct" }
-];
+const models = [{ supportsComplex: true, name: "@cf/meta/llama-3.2-11b-vision-instruct" }];
 
 const apiUrl = "https://proxy.mubilop.tech";
 export let aiModel = models[0];
